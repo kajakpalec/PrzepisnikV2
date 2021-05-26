d11c315b0e81865e9f433e736ad9216a2439afc1
kajakpalec <pawluskatarzyna7@gmail.com>
2021-05-26 21:54:55 +0200

    Updates

---
+++ static/js/main.e7dd5c69.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpprzepisnikv2=this.webpackJsonpprzepisnikv2||[]).push([[0],{10:function(e,n,t){"use strict";t.r(n);var i=t(1),r=t.n(i),c=t(3),s=t.n(c),o=(t(8),t(0));var p=function(){return Object(o.jsx)("div",{children:Object(o.jsx)("h1",{children:"Hello Przepisy"})})};s.a.render(Object(o.jsx)(r.a.StrictMode,{children:Object(o.jsx)(p,{})}),document.getElementById("root"))},8:function(e,n,t){}},[[10,1,2]]]);
+//# sourceMappingURL=main.e7dd5c69.chunk.js.map
